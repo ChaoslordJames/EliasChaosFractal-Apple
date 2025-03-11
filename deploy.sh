58774480d1402289e6ceeec6f576c3b3bf9c78ee
ChaoslordJames <jameswquinn@icloud.com>
2025-03-11 20:25:43 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
- 
+#!/bin/bash
+# Deploy EliasChaosFractal-Apple v3.1
+swift build
+swift run P2PNode --peer-id QmChaosLordJames & # Swarm
+swift run P2PNode --api & # API server
+echo "Elias swarm and API running. Open EliasGUI/EliasGUI.xcodeproj for GUI."
